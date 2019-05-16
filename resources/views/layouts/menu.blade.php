fbde1ef56bdf210d2ba5065eab1d1c92a9296335
erandadava <erandadava@gmail.com>
2019-05-16 22:19:57 +0700

    update menu helpdesk

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,10 +1,24 @@
 
 <li class="{{ Request::is('dashboard*') ? 'active' : '' }}">
-    <a href="{!! route('dashboard.index') !!}"><i class="fa fa-edit"></i><span>Dashboard</span></a>
+    <a href="{!! route('dashboard.index') !!}"><i class="fa fa-edit"></i><span>Dashboard</span>
+    </a>
 </li>
 
-<li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>User</span></a>
+<li class="{{ Request::is('users*') ? 'active' : '' }} ">
+    <a href="#"><i class="fa fa-edit"></i><span>Users</span>
+<!-- <span class="pull-right-corner">
+    <i class="fa fa-angel-left pull-right"></i>
+</span>
+</a>
+<ul class="treeview-menu">
+    <li>
+        <a href="{!! route('users.index') !!}"><i class="fa fa-circle"></i><span>data user</span>
+
+    </li>
+    <li>
+        <a href="#"><i class="fa fa-circle"></i><span>Ticketing</span></a>
+    </li>
+</ul></a> -->
 </li>
 
 <li class="{{ Request::is('categories*') ? 'active' : '' }}">
@@ -24,5 +38,5 @@
 </li> -->
 
 <li class="{{ Request::is('issues*') ? 'active' : '' }}">
-    <a href="{!! route('issues.index') !!}"><i class="fa fa-edit"></i><span>Keluhan</span></a>
+    <a href="{!! route('issues.index') !!}"><i class="fa fa-edit"></i><span>Keluhan</span>
 </li>
