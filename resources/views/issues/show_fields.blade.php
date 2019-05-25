ae011e6c857fc32bce02ba727e6d666b5992a0b5
CalvinJO <cjob280@gmail.com>
2019-05-25 16:10:34 +0700

    edit judul

--- resources/views/issues/show_fields.blade.php
+++ resources/views/issues/show_fields.blade.php
@@ -179,7 +179,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Ajukan ke IT OPS</h4>
+        <h4 class="modal-title">Ajukan ke IT Support</h4>
       </div>
       <div class="modal-body">
         <div class="row">
