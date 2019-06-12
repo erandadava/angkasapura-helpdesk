35c12d3c97968b200ce4eafdd88b7597d6012555
Karel Herman <karel@Karels-MacBook-Pro.local>
2019-06-12 21:14:40 +0700

    update FE

--- resources/views/webuser/konten.blade.php
+++ resources/views/webuser/konten.blade.php
@@ -138,36 +138,79 @@
                 </div>
               </div>
             </div>
-				
-				</div>
+        	</div>
 
 				<div class="row">
-				<div class="col-lg-6 col-md-12">
+				  <div class="col-lg-8 col-md-12">
+            <div class="card">
+              
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">Your Tickets </h4>
+              </div>
+
+              <div class="card-body table-responsive">
+                <table class="table">
+                  <thead>
+                    <th>Priority</th>
+                    <th>Category</th>
+                    <th>Problem</th>
+                    <th>Location</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </thead>
+                  
+                  <tbody>
+                    <tr>
+                      <td><span class="uk-label uk-label-danger">High</span></td>
+                      <td>System & Security</td>
+                      <td>Cpu Kemasukan Oli Buzz, Processor Turbo boost up hingga 2.5 Bar</td>
+                      <td>Terminal 3, Unit 1, Meja ABCD</td>
+                      <td><span class="uk-label">Forwarded to IT Support</span></td>
+                      <td>
+                        <a id="js-modal-prompt" class="uk-button uk-button-default" href="#">Take</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12">
               <div class="card">
-                <div class="card-header card-header-tabs card-header-primary">
-                  <div class="nav-tabs-navigation">
-                    <div class="nav-tabs-wrapper">
-                      <span class="nav-tabs-title">Tickets:</span>
-                      <ul class="nav nav-tabs" data-tabs="tabs">
-                        <li class="nav-item">
-                          <a class="nav-link active" href="#profile" data-toggle="tab">
-                            <i uk-icon="pull"> </i> Uncomplete
-                            <div class="ripple-container"></div>
-                          </a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#messages" data-toggle="tab">
-                            <i uk-icon="check"></i> Completed
-                            <div class="ripple-container"></div>
-                          </a>
-                        </li>	
-                      </ul>
-                    </div>
-                  </div>
-                </div
-				</div>
-
-
-</div>
+                <div class="card-header card-header-warning">
+                  <h4 class="card-title">Tickets History</h4>
+                </div>
+                <div class="card-body table-responsive">
+                  <table class="table table-hover">
+                    <tbody>
+                      <tr>
+                        <td>
+                          <div class="form-check">
+                            <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" value="" checked>
+                            <span class="form-check-sign">
+                              <span class="check"></span>
+                            </span>
+                          </div>
+                        </td>
+                        <td>Cpu Kemasukan Oli Buzz, Processor Turbo boost up hingga 2.5 Bar</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+      </div>
+        
 </body>
+
+<script>
+   UIkit.util.on('#js-modal-prompt', 'click', function (e) {
+           e.preventDefault();
+           e.target.blur();
+           UIkit.modal.prompt('Solution:', 'Your Solution').then(function (solution) {
+               console.log('Prompted:', solution)
+           });
+       });
+</script>
 @endsection
