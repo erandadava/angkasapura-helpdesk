7ea290cce32d1e4201a10f25c0d734f1b3f18a2a
CalvinJO <cjob280@gmail.com>
2019-07-22 13:25:57 +0700

    Show Number Dashboard

--- resources/views/dashboard/script.blade.php
+++ resources/views/dashboard/script.blade.php
@@ -1,5 +1,6 @@
 @section('scripts')
         <script>
+            Chart.plugins.unregister(ChartDataLabels);
             var jumlah_prioritas = {!!$jumlah_prioritas!!};
             var label_prioritas = $.map(jumlah_prioritas, function(e) {
                 return e.prio_name;
@@ -14,7 +15,6 @@
             var value_kategori = $.map(jumlah_kategori, function(e) {
                 return e.count;
             });
-            console.log(jumlah_prioritas);
             var prioritas = new Chart(document.getElementById('chartPrioritas'), {
                 type: 'horizontalBar',
                 data: {
@@ -27,6 +27,59 @@
                         }
                     ]
                 },
+                options : {
+                    scales: {
+                        xAxes: [
+                        {
+                            ticks: {
+                            beginAtZero: true,
+                            },
+                            scaleLabel: {
+                            display: false
+                            },
+                            gridLines: {},
+                            stacked: true
+                        }
+                        ],
+                        yAxes: [
+                        {
+                            stacked: true
+                        }
+                        ]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    animation: {
+                        onComplete: function() {
+                        var chartInstance = this.chart;
+                        var ctx = chartInstance.ctx;
+                        ctx.textAlign = "left";
+                        ctx.font = "11px";
+                        ctx.fillStyle = '#223388';
+                        Chart.helpers.each(
+                            this.data.datasets.forEach(function(dataset, i) {
+                            var meta = chartInstance.controller.getDatasetMeta(i);
+                            Chart.helpers.each(
+                                meta.data.forEach(function(bar, index) {
+                                data = dataset.data[index];
+                                if (i == 0) {
+                                    ctx.fillText(data, 70, bar._model.y + 4);
+                                } else {
+                                    ctx.fillText(data, bar._model.x - 25, bar._model.y + 4);
+                                }
+                                }),
+                                this
+                            );
+                            }),
+                            this
+                        );
+                        }
+                    },
+                    pointLabelFontFamily: "Quadon Extra Bold",
+                    scaleFontFamily: "Quadon Extra Bold"
+                }
+                
             });
             var kategori = new Chart(document.getElementById('chartKategori'), {
                 type: 'pie',
@@ -41,6 +94,15 @@
                     ]
                 },
                 labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                options: {
+                    plugins: {
+                        labels: {
+                            render: 'value',
+                            fontSize: 11,
+                            fontColor: '#fff',
+                        }
+                    }
+                }
             });
             var keluhan = new Chart(document.getElementById('chartKeluhan'), {
                 type: 'doughnut',
@@ -54,10 +116,21 @@
                         }
                     ]
                 },
+                options: {
+                    plugins: {
+                        labels: {
+                            render: 'value',
+                            fontSize: 11,
+                            fontColor: '#fff',
+                        }
+                    }
+                }
+                
                 
             });
             var bulan = new Chart(document.getElementById('chartBulan'), {
                 type: 'line',
+                plugins: [ChartDataLabels],
                 data: {
                     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'],
                     datasets: [
@@ -68,6 +141,33 @@
                         }
                     ]
                 },
+                options: {
+                    plugins: {
+                    datalabels: {
+                        align: function(context) {
+                            var index = context.dataIndex;
+                            var value = context.dataset.data[index];
+                            var invert = Math.abs(value) <= 1;
+                            return value < 1 ? 'end' : 'start'
+                            },
+                            anchor: 'end',
+                            backgroundColor: null,
+                            borderColor: null,
+                            borderRadius: 4,
+                            borderWidth: 1,
+                            color: '#223388',
+                            font: {
+                            size: 11,
+                            weight: 600
+                            },
+                            offset: 4,
+                            padding: 0,
+                            formatter: function(value) {
+                                return Math.round(value * 10) / 10
+                            }
+                        }
+                    }
+                }
             });
         </script>
 @endsection
