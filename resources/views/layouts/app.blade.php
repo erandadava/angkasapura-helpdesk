173ba9c2be3f31738e1033ac4cf935e9b96cf244
CalvinJO <cjob280@gmail.com>
2019-08-30 17:41:27 +0700

    profil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,7 +112,7 @@
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                    <a href="/users/{{Auth::id()}}/edit" class="btn btn-default btn-flat">Ubah Profil dan Password</a>
+                                    <a href="/users/{{Auth::id()}}/edit" class="btn btn-default btn-flat">Profil</a>
                                     </div>
                                     <div class="pull-right">
                                         <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat"
